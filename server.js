812305a12283ac7ea41e9f779b231c264db6f1dc
FFieryL <141057942+FFieryL@users.noreply.github.com>
2025-12-28 01:40:23 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let messages = [];
+const MAX_MESSAGES = 100;
+
+app.post("/send", (req, res) => {
+  const { user, text, channel = "global" } = req.body;
+
+  messages.push({ user, text, channel, time: Date.now() });
+  if (messages.length > MAX_MESSAGES) messages.shift();
+
+  res.sendStatus(200);
+});
+
+app.get("/poll", (req, res) => {
+  const since = Number(req.query.since || 0);
+  res.json(messages.filter(m => m.time > since));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("CT IRC relay running"));
